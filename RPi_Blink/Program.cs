f0cf9f37ca399cd1354ec080faf8b5efb5982710
Kimberly Jansen <kim@xunil.co.nz>
2023-10-01 20:05:01 +1300

    Added Blink for Raspberry Pi to Solution

---
+++ RPi_Blink/Program.cs
@@ -0,0 +1,20 @@
+﻿#include <wiringPi.h>
+
+// LED Pin - wiringPi pin 0 is BCM_GPIO 17.
+
+#define	LED	17
+
+int main(void)
+{
+    wiringPiSetupSys();
+
+    pinMode(LED, OUTPUT);
+
+    while (true)
+    {
+        digitalWrite(LED, HIGH);  // On
+        delay(500); // ms
+        digitalWrite(LED, LOW);   // Off
+        delay(500);
+    }
+    return 0;
